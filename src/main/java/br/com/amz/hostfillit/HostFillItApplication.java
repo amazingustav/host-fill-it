a2cd0562d6bdbb665f6ff16f8c0f1131e680aa1c
Gustavo Amorim <gustavoamorim02@gmail.com>
2024-01-25 22:24:15 -0300

    First commit - Hello, world!

---
+++ src/main/java/br/com/amz/hostfillit/HostFillItApplication.java
@@ -0,0 +1,12 @@
+package br.com.amz.hostfillit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostFillItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostFillItApplication.class, args);
+	}
+}
