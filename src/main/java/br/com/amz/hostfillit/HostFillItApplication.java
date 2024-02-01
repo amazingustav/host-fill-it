d009f7d9b89c5e660d4702947850f0461a78678b
Gustavo Amorim <gustavoamorim02@gmail.com>
2024-01-31 23:43:30 -0300

    chore: enable JPA auditing for createdAt and updatedAt fields

--- src/main/java/br/com/amz/hostfillit/HostFillItApplication.java
+++ src/main/java/br/com/amz/hostfillit/HostFillItApplication.java
@@ -2,8 +2,14 @@ package br.com.amz.hostfillit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableJpaAuditing
 @SpringBootApplication
+@EnableTransactionManagement
+@EntityScan(basePackages = {"br.com.amz.hostfillit.persistence.entity"})
 public class HostFillItApplication {
 
 	public static void main(String[] args) {
